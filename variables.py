5ace77d3b627162438e161b790b760810fca7fb1
ashley3schultz <ashleyeschultz@gmail.com>
2019-02-03 18:17:58 -0700

    Add variables notes

---
+++ variables.py
@@ -0,0 +1,26 @@
+variable = "lower_case or _lower_case"
+_class = "CamelCase"
+constant = "SNAKE_CASE"
+boolean_t = True
+boolean_f = False
+null_nil = "None"
+escape = "use \\ to escape"
+concatenation = "this" + " and " + "that"
+numi = 9
+nums = "9"
+use_int_in_strings = f"{str(numi)} years old"
+use_str_number =  (1 + int(nums))
+interpolation = f"You are {numi} years old"
+_input = input("what's your name:")
+
+print(variable)
+print(_class)
+print(constant)
+print(boolean_t)
+print(boolean_f)
+print(null_nil)
+print(escape)
+print(concatenation)
+print(use_int_in_strings)
+print(use_str_number)
+print(interpolation)
