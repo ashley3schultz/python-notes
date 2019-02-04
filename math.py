e1920c0dd1b3c8e95cb27ab48cbb3dec285279f5
ashley3schultz <ashleyeschultz@gmail.com>
2019-02-03 18:18:26 -0700

    Add math notes

---
+++ math.py
@@ -0,0 +1,14 @@
+import math 
+
+print(95+5)    #Additon = 100
+print(105-5)   #Subtraction = 100
+print(5*20)    #Multiplecation = 100
+print(10**2)   #Power of = 100
+print(200/2)   #Divition to float = 100.0
+print(200//2)  #Divition to int = 100
+print(500%400) #Remainder = 100
+print(round(8.4636, 2))
+
+#uses math import
+print(math.ceil(8.4)) 
+print(math.floor(8.6))
