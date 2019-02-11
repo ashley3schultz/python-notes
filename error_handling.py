784a1789e1e8276ec5ba30bd241442d9f10d9044
ashley3schultz <ashleyeschultz@gmail.com>
2019-02-10 18:38:24 -0700

    Add notes on error raising

---
+++ error_handling.py
@@ -0,0 +1,8 @@
+try:
+    raise Exception('You raised an error')
+except:
+    raise Exception('You raised an error')
+else: 
+    raise Exception('You raised an error')
+finally:
+    raise Exception('You raised an error')
